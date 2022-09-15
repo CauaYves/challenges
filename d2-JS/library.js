aebab4e2cdec073012209fbce612ee2cff971e6d
HOLLOWYVES <cauayves5@gmail.com>
2022-09-15 12:47:57 -0300

    desafio 02

---
+++ d2-JS/library.js
@@ -0,0 +1,5 @@
+function qualONome(){
+ var nome = window.prompt('Qual o seu nome?')
+ var idade = window.prompt('Olá, ' + nome + '!  Quantos anos você tem?')
+ window.alert('Acabei de conhecer ' + nome + ', que tem ' + idade + ' anos de idade!')
+}
