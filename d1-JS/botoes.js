0f8c19fdda2f5a9cc59aa51db7058bba7278dcba
HOLLOWYVES <cauayves5@gmail.com>
2022-09-15 12:21:24 -0300

    desafio do curso de javascript

---
+++ d1-JS/botoes.js
@@ -0,0 +1,9 @@
+function clicar1(){
+    window.alert('voçê clicou no botão 1')
+}
+function clicar2(){
+    window.alert('você clicou no botão 2')
+}
+function clicar3(){
+    window.alert('voce clicou no botão 3')
+}
