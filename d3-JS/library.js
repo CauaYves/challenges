c3d5cafedbbdca5281b6f63d5e7bbec3f8e7a164
HOLLOWYVES <cauayves5@gmail.com>
2022-09-17 23:28:42 -0300

    d3-JS

---
+++ d3-JS/library.js
@@ -0,0 +1,11 @@
+function antesEDepois() {
+
+    var n = prompt('Digite um numero inteiro qualquer')
+
+    var number = Number(n)
+    var numbermen = number - 1
+    var numbermai = number + 1
+
+    alert(`antes de ${number}, temos o número ${numbermen}. \n Depois de ${number}, temos o número ${numbermai}`)
+    
+}
