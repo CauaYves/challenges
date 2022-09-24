745ee8b496335a698a22b7ba2205c150e12f175b
HOLLOWYVES <cauayves5@gmail.com>
2022-09-24 11:15:55 -0300

    d11-JS

---
+++ d11-JS/library.js
@@ -0,0 +1,5 @@
+function verificarAnoBissexto () {
+    let anoInserido = parseInt(prompt(`Qual é o ano quer você precisa verificar?`))
+    let verificacao = anoInserido / 4
+    if (verificacao )
+}
